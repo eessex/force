cea2024e20db3ea01160392f2739690e819a9e31
Eve Essex <eve.essex@gmail.com>
2018-04-11 16:14:50 -0400

    add babel-plugin-rewire

--- src/desktop/apps/article/components/InfiniteScrollArticle.js
+++ src/desktop/apps/article/components/InfiniteScrollArticle.js
@@ -5,13 +5,10 @@ import Waypoint from 'react-waypoint'
 import styled from 'styled-components'
 import colors from 'reaction/Assets/Colors'
 import { data as sd } from 'sharify'
-import { positronql as _positronql } from 'desktop/lib/positronql'
 import { Article } from 'reaction/Components/Publishing'
 import { articlesQuery } from 'desktop/apps/article/queries/articles'
 import { setupFollows, setupFollowButtons } from './FollowButton.js'
-
-// FIXME: Rewire
-let positronql = _positronql
+import { positronql } from 'desktop/lib/positronql'
 
 const FETCH_TOP_OFFSET = 200
 
